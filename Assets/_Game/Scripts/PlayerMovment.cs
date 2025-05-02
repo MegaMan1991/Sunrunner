579145717e02977bdd3c1d2cff5fa6244d0fe225
MegaMan1991 <82349474+MegaMan1991@users.noreply.github.com>
2025-05-01 23:46:12 -0500

    May 1st Test
    May 1st Test

--- Assets/_Game/Scripts/PlayerMovment.cs
+++ Assets/_Game/Scripts/PlayerMovment.cs
@@ -4,11 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovment : MonoBehaviour
 {
     Vector2 movment;
-    public float _moveSpeed = 1;
-    public  float _flyTime = 5;
+    [SerializeField] float _moveSpeed = 1;
     private Rigidbody2D _rb;
-    private float count = 0;
-
 
     public void inputPlayer(InputAction.CallbackContext _context)
     {
@@ -18,30 +15,11 @@ public class PlayerMovment : MonoBehaviour
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        count = _flyTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = -movment.x;
-        float y = movment.y;
-        count += Time.deltaTime;
-        if (count > _flyTime || count < 1) 
-        {
-            y = 0;
-        }
-
-        _rb.MovePosition(_rb.position + (new Vector2(x, y).normalized * _moveSpeed));
-    }
-    
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-
-        if (this.transform.position.y > collision.transform.position.y && _flyTime != float.MaxValue) 
-        {
-            count = (count < _flyTime ? 1 : 0);
-        }
+        _rb.MovePosition(_rb.position + (new Vector2(-movment.x, movment.y).normalized * _moveSpeed));
     }
 }
